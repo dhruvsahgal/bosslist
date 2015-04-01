b3ef80c9efb683127fb5d6065c8a0b5dab6cd394
dhruvsahgal <dhruvsahgal@users.noreply.github.com>
2015-04-02 01:44:37 +0800

    Pomodoro.js

---
+++ pomodoro.js
@@ -0,0 +1,24 @@
+function Pomodoro() {
+	var intervalTime = document.getElementById('intervalTime');
+	var pomodori = 1;
+	var pomodoriMax = 4;
+	var breakTime = 5;
+	var breakTimeMax = 30;
+
+	
+	start timer using "intervalTime" as countdown
+	While timer is running {
+		pomodori = pomodori + 1;
+
+		if pomodori > pomodoriMax 
+		{
+			breakTime= breakTime;
+		else {
+			breakTime = breakTimeMax;
+			}
+		}
+
+	}
+
+
+}
